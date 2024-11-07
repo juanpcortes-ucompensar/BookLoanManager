f544a30b46346a1adb2892395fbadeb52d36a028
Pablo Cortes <pcortes@cobiscorp.com>
2024-11-07 17:19:24 -0500

    Se instalan los proyectos de spring y de react.

---
+++ bookloanspring/src/main/java/com/example/bookloanspring/BookLoanManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookloanspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLoanManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLoanManagerApplication.class, args);
+	}
+
+}
---
+++ bookloanspring/src/test/java/com/example/bookloanspring/BookLoanManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookloanspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLoanManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
